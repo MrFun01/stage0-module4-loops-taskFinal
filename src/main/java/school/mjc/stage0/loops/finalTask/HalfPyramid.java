731d02c4b877bc17d563dd3fa4dac3e387070eb6
MrFun01 <hprobook48@gmail.com>
2023-04-12 16:01:21 +0500

    Finished Final task without Pyramid

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        // I won't know  who will read this comment anybody or not . I do my best.
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = i; j < cathetusLength ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
 
